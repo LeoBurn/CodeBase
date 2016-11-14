642dcb716abf72d212eaf6cf1feb65b88e044eaa
Leandro Viana <leandro.viana@celfinet.com>
2016-11-14 18:12:32 +0000

    Add Singleton pattern

---
+++ Patterns.Tests/PatternsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Singleton.Pattern;
+
+namespace Patterns.Tests
+{
+  [TestClass]
+  public class PatternsTests
+  {
+    [TestMethod]
+    public void ShouldTestSingleTonPatternSingleThread()
+    {
+      //Arrange
+      Service serviceOne, serviceTwo;
+      int serviceOneHashCode, serviceTwoHashCode;
+
+      //Act
+      serviceOne = Service.Instance;
+      serviceTwo = Service.Instance;
+
+      serviceOneHashCode = serviceOne.GetHashCode();
+      serviceTwoHashCode = serviceTwo.GetHashCode();
+
+      //Assert
+      Assert.AreEqual(serviceOneHashCode,serviceTwoHashCode);
+    }
+  }
+}
---
+++ Singleton.Pattern/Service.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace Singleton.Pattern
+{
+    public class Service
+    {
+      private static object syncRoot = new object();
+      private static volatile Service instance;
+
+      public static Service Instance
+      {
+        get
+        {
+          //DoubleCheck Null because Multi-Threading
+          if (instance == null)
+          {
+            lock (syncRoot)
+            {
+              if(instance == null)
+              instance = new Service();
+            }
+          }
+          return instance;
+        }
+      }
+    }
+}
