1b555d24b1971694ab54204cd751855d2f8da7c8
Leandro Viana <leandro.viana@celfinet.com>
2016-11-21 22:18:32 +0000

    Bubble Sort Implementation and UnitTest Of Swap Method

---
+++ Algorithms.Tests/SortingAlgorithmsTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sorting.Algortihms;
+
+namespace Algorithms.Tests
+{
+  [TestClass]
+  public class SortingAlgorithmsTest
+  {
+    [TestMethod]
+    public void ShouldSwapValuesInBubleSort()
+    {
+      //Arrange
+      BubleSort bubleSort = new BubleSort();
+      int n1 = 100, n2 = 150;
+      int expectedn1 = 150, expectedn2 = 100;
+      //Act
+      bubleSort.Swap(ref n1, ref n2);
+
+      //Assert
+      Assert.AreEqual(n1,expectedn1);
+      Assert.AreEqual(n2, expectedn2);
+    }
+
+    [TestMethod]
+    public void ShouldSortWithBubleSort()
+    {
+      //Arrange
+      BubleSort bubleSort = new BubleSort();
+      int n1 = 100, n2 = 150;
+      int expectedn1 = 150, expectedn2 = 100;
+      //Act
+      bubleSort.Swap(ref n1, ref n2);
+
+      //Assert
+      Assert.AreEqual(n1, expectedn1);
+      Assert.AreEqual(n2, expectedn2);
+    }
+  }
+}
---
+++ Sorting.Algortihms/Bu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting.Algortihms
+{
+    public class BubleSort
+    {
+      public void Sort(int[] arr)
+      {
+        for (int j = arr.Length - 1; j >= 0; --j)
+        {
+          for (int i = 0; i < j; i++)
+          {
+            if(arr[i] > arr[i+1])
+              Swap(ref arr[i],ref arr[i+1]);
+          }
+        }
+      }
+
+      public void Swap(ref int n1, ref int n2)
+      {
+        if (n1 == n2) return;
+        n1 ^= n2;
+        n2 ^= n1;
+        n1 ^= n2;
+      }
+
+    }
+}
